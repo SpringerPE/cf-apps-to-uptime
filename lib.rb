88ab4f1d92140298383723ce6cc33b018dd7f5aa
Felix Friedrich <felix.friedrich@springer.com>
2015-02-18 16:04:52 +0100

    improved solution for issue #4
    Conflicts:
    	lib.rb

--- lib.rb
+++ lib.rb
@@ -27,7 +27,7 @@ def get_meta(url)
   end
 end
 
-def enhance_app_data(app, meta_path, regex)
+def enhance_app_data(app, meta_path, regex, alert_threshold, interval)
   entry_url = app["routes"].select {|route| should_monitor_route? route, regex }[0]
   if not /^http:\/\//.match entry_url
     entry_url = "http://#{entry_url}"
@@ -35,6 +35,8 @@ def enhance_app_data(app, meta_path, regex)
   meta_url = File.join(entry_url, meta_path)
   app["monitor_routes"] = [meta_url]
   app["meta"] = get_meta(meta_url)
+  app["alertTreshold"] = alert_threshold if not alert_threshold.nil?  # keyword 'alertTreshold' is misspelled, because it is misspelled in Uptime
+  app["interval"] = interval if not interval.nil?
   app
 end
 
@@ -76,8 +78,6 @@ def add_to_uptime(data, uptime_api)
   body = {"name" => data['url'],
           "url"  => data['url'],
           "tags" => tags.flatten}
-  body["alertTreshold"] = AppConfig::ALERT_TRESHOLD if not AppConfig::ALERT_TRESHOLD.nil?
-  body["interval"] = AppConfig::INTERVAL if not AppConfig::INTERVAL.nil?
   response = HTTParty.put(uptime_api, :body => body)
 end
 
